3edf23c689193a4f08a18b6880905040fd109be3
Piotr Górski <piotrek.gorski90@gmail.com>
2017-11-07 14:10:45 +0100

    User database work with user models now

--- user_database.py
+++ user_database.py
@@ -1,16 +1,18 @@
 import csv
+import user_models
 user_list = []
 
 def read_from_csv(filename = 'user_data.csv'):
     with open(filename, newline='') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-            if row[0] == 'manager':
-                user_list.append(Manager(*row)) #zamienic Manager na dobrą nazwę
-            elif row[0] == 'employee':
-                user_list.append(Employee(*row)) #zamienic Employee na dobrą nazwę
-            elif row[0] == 'mentor':
-                user_list.append(Mentor(*row))  # zamienic Mentor na dobrą nazwę
-            else: #Else czy elif ?
-                user_list.append(Student(*row))  # zamienic Student na dobrą nazwę
+            rank = row[0]
+            if rank == 'manager':
+                user_list.append(user_models.Manager(*row[1:])) #zamienic Manager na dobrą nazwę
+            elif rank == 'employee':
+                user_list.append(user_models.Employee(*row[1:])) #zamienic Employee na dobrą nazwę
+            elif rank == 'mentor':
+                user_list.append(user_models.Mentor(*row[1:]))  # zamienic Mentor na dobrą nazwę
+            elif rank == 'student':
+                user_list.append(user_models.Student(*row[1:]))  # zamienic Student na dobrą nazwę
 read_from_csv()
