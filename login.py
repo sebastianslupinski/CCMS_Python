6728e635df85a31051ca9df83ae44f6d5730108b
przemekNachel <przemeknachel@gmail.com>
2017-11-07 13:06:56 +0100

    Add login system

---
+++ login.py
@@ -0,0 +1,37 @@
+import getpass
+
+class User:
+
+    def __init__(self, rank, login, password):
+        self.rank = rank
+        self.login = login
+        self.password = password
+
+user_list = []
+
+
+def get_user_by_login(login):
+    for user in user_list:
+        if user.login == login:
+            return user
+
+def login():
+    while True:
+        username = input("Type username\n")
+        user = get_user_by_login(username)
+        if user:
+            password = getpass.getpass("Type password\n")
+            if user.password == password:
+                return True
+
+
+
+
+def read_csv():
+    with open("user_data.csv.txt") as file:
+        for line in file:
+            data = line.split(",")
+            user_list.append(User(data[0], data[1], data[2]))
+
+read_csv()
+login()
