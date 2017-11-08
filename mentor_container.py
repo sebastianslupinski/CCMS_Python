d4889f55a80217a7f0ded300ebb5e3647558e57f
Piotr Górski <piotrek.gorski90@gmail.com>
2017-11-08 14:28:27 +0100

    Add add/remove methods

--- mentor_container.py
+++ mentor_container.py
@@ -3,11 +3,11 @@ class MentorContainer:
     def __init__(self):
         self.mentor_list = []
 
-    def add_mentor(self):
-        pass
+    def add_mentor(self, user):
+        self.mentor_list.append(user)
 
     def edit_mentor(self):
         pass
 
-    def remove_mentor(self):
-        pass
+    def remove_mentor(self, index):
+        self.mentor_list.pop(index)
