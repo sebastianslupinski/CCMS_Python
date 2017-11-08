1f9eb714e2ea2a818e2dee9fd80ecc1b4573aeec
sebastianslupinski <bezerkey@gmail.com>
2017-11-08 15:23:39 +0100

    add view_mentor.py

---
+++ view_mentor.py
@@ -0,0 +1,14 @@
+class ViewMentor:
+
+    @staticmethod
+    def input_student_info():
+
+        login = input("Please enter student's login: ")
+        password = input("Please enter password: ")
+        name = input("Please enter student's name: ")
+        surname = input("Please enter student's surname: ")
+        phone_number = input("Please enter student's phone number")
+
+        return login, password, name, surname, phone_number
+
+    
