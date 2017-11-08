6d5f4a266f80eaa81cb7898cfc4aa8e6d41f04ef
Piotr Górski <piotrek.gorski90@gmail.com>
2017-11-08 14:29:24 +0100

    Add add/remove methods

--- student_container.py
+++ student_container.py
@@ -3,11 +3,11 @@ class StudentContainer:
     def __init__(self):
         self.student_list = []
 
-    def add_student(self):
-        pass
+    def add_student(self,user):
+        self.student_list.append(user)
 
     def edit_student(self):
         pass
 
-    def remove_student(self):
-        pass
+    def remove_student(self, index):
+        self.item_list.pop(index)
