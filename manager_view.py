42ad17018535b5688711ea1e1982ebb1b2d1d089
Piotr Górski <piotrek.gorski90@gmail.com>
2017-11-09 10:41:42 +0100

    Add menu for manager controller

---
+++ manager_view.py
@@ -0,0 +1,28 @@
+# import mentor_container
+# import user_database
+
+class ViewManager:
+
+    @staticmethod
+    def get_user_input(message):
+        return input(message)
+
+    @staticmethod
+    def input_mentor_info():
+
+        login = input("Please enter mentor's login: ")
+        password = input("Please enter password: ")
+        name = input("Please enter mentor's name: ")
+        surname = input("Please enter mentors's surname: ")
+        phone_number = input("Please enter mentors's phone number: ")
+
+        return login, password, name, surname, phone_number
+
+    @classmethod
+    def select_edit_option(cls):
+        edit_commands = ('Change name', 'Change surname', 'Change password', 'Change phone number', 'Go back')
+        for option in edit_commands:
+            print(str(edit_commands.index(option) + 1) + "----->" + option)
+        choosen_option = cls.get_user_input('Choose: ')
+        return choosen_option
+
