2d16c1ffe5d01b8bc2a32b029b00a91ec22e8943
przemekNachel <przemeknachel@gmail.com>
2017-11-20 11:30:38 +0100

    Temportary change getchar to input

--- root_controller_view.py
+++ root_controller_view.py
@@ -16,15 +16,16 @@ class RootControllerView:
 
     @staticmethod
     def getch():
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
+        # import sys, tty, termios
+        # fd = sys.stdin.fileno()
+        # old_settings = termios.tcgetattr(fd)
+        # try:
+        #     tty.setraw(sys.stdin.fileno())
+        #     ch = sys.stdin.read(1)
+        # finally:
+        #     termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        # return ch
+        return input("")
 
     @staticmethod
     def greet_user():
