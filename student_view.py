e32c735bcf1fecb169a565d6fd5a0d53346d759e
Piotr Górski <piotrek.gorski90@gmail.com>
2017-11-22 14:26:00 +0100

    Add answering to description and acces to assignments by required users

--- student_view.py
+++ student_view.py
@@ -15,3 +15,15 @@ class StudentView(View):
         student_commands = ('Display grades', 'Submit assignment')
         cls.display_menu(student_commands)
         return cls.get_user_input('Choose: ')
+
+    @staticmethod
+    def show_assignments(assignments):
+        View.clear_terminal()
+        print('Your assignments: \n')
+        for assignment in assignments:
+            print('Title: ' + assignment.title + '\n' + 'Grade: ' + str(assignment.grade) + '\n')
+
+    @staticmethod
+    def submit_assignment(assignment):
+        print('Title: ' + assignment.title + '\n\n' + 'Description: ' + assignment.description + '\n\n')
+        return input('Enter your answer below:\n\n')
